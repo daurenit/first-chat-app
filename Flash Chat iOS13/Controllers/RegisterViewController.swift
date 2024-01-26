ca0b6c864c1aa1ad6b9e95730d2b1ec203ac9d50
Dauren <dauren09s1@mail.ru>
2024-01-26 16:15:43 +0600

    Completed Project

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class RegisterViewController: UIViewController {
-
+final class RegisterViewController: UIViewController {
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+//        if #available(iOS 14.0, *) {
+//            let logger = Logger()
+//        } else {
+//            // Fallback on earlier versions
+//        }
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+            
+        }
     }
     
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 22807 on 18.10.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //users
+    let body: String
+}
